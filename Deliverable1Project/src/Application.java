398ddb5497d323343be76a1521171e315fbc3db1
aicavin <aicavin@gmail.com>
2018-01-15 12:12:54 -0500

    add comments

--- Deliverable1Project/src/Application.java
+++ Deliverable1Project/src/Application.java
@@ -3,39 +3,38 @@ import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		doTask();
+		doTask(); // do task in a separate method
 	}
 	public static void doTask() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter two numbers: ");
+		Scanner input = new Scanner(System.in); // create scanner object
+		System.out.println("Enter two numbers: "); // collect input from user
 		String str1 = input.next();
 		String str2 = input.next();
 		
 		// validate numbers have same # digits before checking anything else
 		if (str1.length() != str2.length()) {
-			System.out.println("Numbers must be the same length.");
+			System.out.println("Numbers must be the same length. Please try again.");
 			return;
 		}
-		int sumInt = -1;
+		int sumInt = -1; //declare and initialize a variable for the sum of digits
 		
-		for (int i = 0; i < str1.length(); ++i) {
-			char c1 = str1.charAt(i);
+		for (int i = 0; i < str1.length(); ++i) {  //use a for loop to cycle through each digit
+			char c1 = str1.charAt(i);  // collect the first character of str1, to see if a digit
 			if (!Character.isDigit(c1)) {
-				System.out.println("Idiot!");
+				System.out.println("Input must be a number. Please try again.");
 				return;
 			}
 			
-			char c2 = str2.charAt(i);
+			char c2 = str2.charAt(i);  // repeat for str2
 			if (!Character.isDigit(c2)) {
-				System.out.println("Idiot!");
+				System.out.println("Input must be a number. Give it another go.");
 				return;
 			}
 		 
-			if (i == 0) {
+			if (i == 0) { //for the first index position, collect the sum of the characters, after conversion to string, then parse to integer
 				sumInt = Integer.parseInt(Character.toString(c1)) + Integer.parseInt(Character.toString(c2));
 			}
-			if (sumInt == Integer.parseInt(Character.toString(c1)) + Integer.parseInt(Character.toString(c2))) {
+			if (sumInt == Integer.parseInt(Character.toString(c1)) + Integer.parseInt(Character.toString(c2))) { // compare each set to the first
 
 			}
 			else {
