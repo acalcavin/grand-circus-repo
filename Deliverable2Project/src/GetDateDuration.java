e0b7ffaf83456a80838bfb66acd4d9583f91b79e
aicavin <aicavin@gmail.com>
2018-01-15 20:59:03 -0500

    finish program and add comments

--- Deliverable2Project/src/GetDateDuration.java
+++ Deliverable2Project/src/GetDateDuration.java
@@ -1,5 +1,9 @@
+import java.lang.reflect.Array;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.YearMonth;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -8,47 +12,89 @@ import java.util.Scanner;
 public class GetDateDuration {
 
 	public static void main(String[] args) throws ParseException {
-	    
-		int year1;
-		int month1;
-		int day1;
-		int year2;
-		int month2;
-		int day2;
-		
-		// TODO: create scanner, input date string, validate
-		
 		Scanner input = new Scanner(System.in);
 		
-		//Get input 
-		System.out.println("Please enter two dates: YYYY MM DD ");
-		year1 = input.nextInt();
-		System.out.println("Year is: " + year1);
-		month1 = input.nextInt();
-		day1 = input.nextInt();
-		System.out.println("Month is: " + month1);
-		
-		
-		
-		/*
-		Calendar cal1 = new GregorianCalendar();
-		Calendar cal2 = new GregorianCalendar();
+		System.out.println("Please enter two dates in this format: MM/DD/YYYY. Hit enter after each date.");
 
-	    SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+		//String input1 = input.nextLine();
+		//String input2 = input.nextLine();
+		String input1 = "22/30/2000";
+		String input2 = "1/02/2000";
+		int d1, m1, yr1, d2, m2, yr2;
+		
+		try {
+		
+			String[] dateParts = input1.split("/");
+			if (Array.getLength(dateParts)!=3) {
+				System.out.println("incorrect date format, must be MM/DD/YYYY");
+				return;
+			}
+			// TODO: ensure dateParts[0] is 2 characters before turning into int
+			// ... do this with the other integer fields
+			d1 = Integer.parseInt(dateParts[1]);
+			m1 = Integer.parseInt(dateParts[0]);
+			yr1 = Integer.parseInt(dateParts[2]);
+			
+			dateParts = input2.split("/");
+			if (Array.getLength(dateParts)!=3) {
+				System.out.println("incorrect date format, must be MM/DD/YYYY");
+				return;
+			}
+			d2 = Integer.parseInt(dateParts[1]);
+			m2 = Integer.parseInt(dateParts[0]);
+			yr2 = Integer.parseInt(dateParts[2]);
+		} catch (Exception e) {
+			System.out.println("date parts are not valid numbers");
+			return;
+		}
+		
+		//check first date if month between 01 and 12; if not, output, "Invalid month"; if yes, repeat for second date
+		if ((m1 <= 0) || (m1 >= 12)) {
+			System.out.println("Invalid month");
+			return;
+		}
+		//repeat for second date
+		if ((m2 <= 0) || (m2 >= 12)) {
+			System.out.println("Invalid month");
+			return;
+		}
+		
+		//compute for first date: # days in input month for corresponding year
+		YearMonth yearMonthObject = YearMonth.of(yr1, m1); //imported java.time
+		int daysInMonth = yearMonthObject.lengthOfMonth(); //28
+		System.out.println(daysInMonth);
+		
+		//check: for year1 and month1, is day1 valid? If no, "Invalid # of days"; If yes, repeat for second date. 
+		if ((d1 <= 0) || (d1 > daysInMonth)) {
+			System.out.println("Invalid number of days");
+			return;
+		}
+		//repeat for second date
+		YearMonth yearMonthObject2 = YearMonth.of(yr2, m2); //imported java.time
+		int daysInMonth2 = yearMonthObject2.lengthOfMonth(); //28
+		System.out.println(daysInMonth2);		
+		
+		if ((d2 <= 0) || (d2 > daysInMonth2)) {
+			System.out.println("Invalid number of days");
+			return;
+		}
+		
+		LocalDate date1 = LocalDate.of(yr1, m1, d1);
+		LocalDate date2 = LocalDate.of(yr2, m2, d2);
+		
+		// gives us our period object based on the 2 local dates
+		Period period = Period.between(date1, date2);
+		String finalYears = Integer.toString(period.getYears());
+		String finalMonths = Integer.toString(period.getMonths());
+		String finalDays = Integer.toString(period.getDays());
+		
+		// print program output to console
+		System.out.print("difference years: " + finalYears + ", months: " +  finalMonths + ", days: " + finalDays);
+				
+		
+		
+		
 
-	    Date date1 = sdf.parse("your first date");
-	    cal1.setTime(date)
-	    Date date2 = sdf.parse("your second date");
-	    cal2.setTime(date);
-	    
-	    date1.compareTo(date2) // 0 if equal, less than 0 if date1 is before, gt 0 if date1 is after
-	     
-	    date1.getDay()
-	    
-	    cal1.getActualMaximum(arg0)
-	    
-	    */
 	}
 	
-
 }
